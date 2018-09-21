9b8211aba4d768f29e17360ba9ccfea252ef7380
zhiyong.qiao <zhiyong.qiao@finlabtech.com>
2018-09-21 14:25:46 +0800

    aa

--- src/main/java/com/example/feign/FeignApplication.java
+++ src/main/java/com/example/feign/FeignApplication.java
@@ -1,11 +1,10 @@
 package com.example.feign;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
@@ -23,8 +22,9 @@ public class FeignApplication {
     public static void main(String[] args) {
         SpringApplication.run(FeignApplication.class, args);
     }
+
     //服务名不区分大小写
-    @FeignClient(value = "helloService" ,fallback = HelloError.class)
+    @FeignClient(value = "helloService", fallback = HelloError.class)
     public interface HelloService {
         @RequestMapping(value = "/hello", method = RequestMethod.GET)
         public String hello(@RequestParam("name") String name);
@@ -34,19 +34,29 @@ public class FeignApplication {
     private HelloService helloService;
 
     @RequestMapping("/hello")
-    public String hello(@RequestParam("name")String name){
+    public String hello(@RequestParam("name") String name) {
         return helloService.hello(name);
     }
 
     @Component
     public class HelloError implements HelloService {
         @Override
-        public String hello(String name){
-            return "hello  ,"+name+"!  sorry ,error !";
+        public String hello(String name) {
+            return "hello  ," + name + "!  sorry ,error !";
         }
     }
 
+    @Value("${name}")
+    private String name;
+    @Value("${age}")
+    private String age;
+    @Value("${version}")
+    private String version = "qiao";
 
+    @RequestMapping("/test")
+    public String test() {
+        return "你好，我是" + name + ",年龄：" + age + "岁。当前环境：" + version;
+    }
 
 
 }
