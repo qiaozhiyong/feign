7edf0a367f6225a707adaba731f9450b0523febb
zhiyong.qiao <zhiyong.qiao@finlabtech.com>
2018-09-21 11:05:15 +0800

    qiao

--- src/main/java/com/example/feign/FeignApplication.java
+++ src/main/java/com/example/feign/FeignApplication.java
@@ -1,11 +1,14 @@
 package com.example.feign;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,7 +24,7 @@ public class FeignApplication {
         SpringApplication.run(FeignApplication.class, args);
     }
     //服务名不区分大小写
-    @FeignClient("helloservice")
+    @FeignClient(value = "helloService" ,fallback = HelloError.class)
     public interface HelloService {
         @RequestMapping(value = "/hello", method = RequestMethod.GET)
         public String hello(@RequestParam("name") String name);
@@ -34,4 +37,16 @@ public class FeignApplication {
     public String hello(@RequestParam("name")String name){
         return helloService.hello(name);
     }
+
+    @Component
+    public class HelloError implements HelloService {
+        @Override
+        public String hello(String name){
+            return "hello  ,"+name+"!  sorry ,error !";
+        }
+    }
+
+
+
+
 }
